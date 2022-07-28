235830e4b89fdecf5d00ad595e0a0dee617ac178
Kenneth Thomsen <contact@systemedz.com>
2022-07-28 13:38:36 +0200

    Changes to quick setup

--- quickinstall_ubuntu2204.sh
+++ quickinstall_ubuntu2204.sh
@@ -5,16 +5,16 @@ if [ $(id -u) != "0" ]; then
 else
 
 # INITIAL SETUP - Ubuntu 22.04 - REQUIRED
-echo "Running initial setup ..." && curl -fsSL https://raw.githubusercontent.com/systemedz/server-automation-scripts/develop/initial-setup_ubuntu2204.sh -o /tmp/initial-setup_ubuntu2204.sh >
+echo "Running initial setup ..." && curl -fsSL https://raw.githubusercontent.com/systemedz/server-automation-scripts/develop/initial-setup_ubuntu2204.sh -o /tmp/initial-setup_ubuntu2204.sh && sh /tmp/initial-setup_ubuntu2204.sh
 
 # Docker Compose
-#echo "Installing Docker & Docker Compose ..." && curl -fsSL https://raw.githubusercontent.com/systemedz/server-automation-scripts/develop/docker-dockercompose_ubuntu2204.sh -o /tmp/docker->
+#echo "Installing Docker & Docker Compose ..." && curl -fsSL https://raw.githubusercontent.com/systemedz/server-automation-scripts/develop/docker-dockercompose_ubuntu2204.sh -o /tmp/docker-dockercompose_ubuntu2204.sh && sh /tmp/docker-dockercompose_ubuntu2204.sh
 
 # Nginx + PHP 8.0 FPM
-#echo "Installing Nginx + PHP8.0-FPM ..." && curl -fsSL https://raw.githubusercontent.com/systemedz/server-automation-scripts/develop/nginx_php8-fpm_ubuntu2204.sh -o /tmp/nginx_php8-fpm_ubu>
+#echo "Installing Nginx + PHP8.0-FPM ..." && curl -fsSL https://raw.githubusercontent.com/systemedz/server-automation-scripts/develop/nginx_php8-fpm_ubuntu2204.sh -o /tmp/nginx_php8-fpm_ubuntu2204.sh && sh /tmp/nginx_php8-fpm_ubuntu2204.sh
 
 # MariaDB
-#echo "Installing MariaDB ..." && curl -fsSL https://raw.githubusercontent.com/systemedz/server-automation-scripts/develop/mariabdb_ubuntu2204.sh -o /tmp/mariadb_ubuntu2204.sh && sh /tmp/ma>
+#echo "Installing MariaDB ..." && curl -fsSL https://raw.githubusercontent.com/systemedz/server-automation-scripts/develop/mariabdb_ubuntu2204.sh -o /tmp/mariabdb_ubuntu2204.sh && sh /tmp/mariabdb_ubuntu2204.sh
 
 # Reboot server - REQUIRED
 #echo "Rebooting..." && reboot
